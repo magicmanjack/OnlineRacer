88b62467aa44c6af724745b2b5f66646358f0902
magicalmanjack <magicalmanjack@gmail.com>
2025-06-21 01:28:46 +1200

    Input system tested on high workload. Produces correct timestamps, and input handles the same for lag and non lag.

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ const MS_PER_UPDATE = 1000 / UPDATES_PER_SECOND;
 
 let gl;
 
+let tires;
+let block;
+
 let camera = new Camera([0, 0, 60.0], [0, 0, 0]); 
 
 if(glSetup()) {
@@ -27,6 +30,39 @@ function glSetup() {
 }
 
 function init() {
+
+    tires = new Array(300);
+    block = new SceneNode();
+    block.mesh = new Mesh(gl, ["models/cube.obj"], "textures/cubetexture.png");
+    block.scaleBy(5, 5, 5);
+    
+    sceneGraph.root.addChild(block);
+
+    function rand() {
+        let num = Math.floor(Math.random() * 100);
+        return num - 50;
+    }
+    
+    for(let i = 0; i < tires.length; i++) {
+        tires[i] = new SceneNode();
+        tires[i].mesh = new Mesh(gl, ["models/wheel.obj"], "textures/wheel.png");
+        sceneGraph.root.addChild(tires[i]);
+        tires[i].translate(rand(), rand(), rand() - 50);
+        tires[i].rotate(rand(), rand(), rand());
+        tires[i].scaleBy(5, 5, 5);
+    }
+
+    /*
+    tires.forEach((tire) => {
+        tire.mesh = new Mesh(gl, ["models/wheel.obj"], "textures/wheel.png");
+        sceneGraph.root.addChild(tire);
+        tire.translate(0, 0, 0);
+        
+        //tire.rotate(rand(), rand(), rand());
+        
+    });
+    */
+
     requestAnimationFrame(render);
 }
 
@@ -47,11 +83,27 @@ function update(timestamp) {
             let frameTimeStamp = timestamp - ((delta - 1) * MS_PER_UPDATE);
             // e.g if delta = 1 that means at the timestamp is this instant. 
             // If delta = 2, the first frame is at (now - 1 * MS_PER_UPDATE).
+            // If delta = 1.5, the first frame occured (now - 0.5 * MS_PER_UPDATE) ago.
             
 
             input.processTimeframeEvents(frameTimeStamp);
-
             //Update objects here
+            tires.forEach((tire) => {
+                tire.rotate(0.1, 0.5, 0.2);
+            });
+
+            if(input.up) {
+                block.translate(0, 0, -1.0);
+            }
+            if(input.down) {
+                block.translate(0, 0, 1.0);
+            }
+            if(input.left) {
+                block.translate(-1.0, 0, 0);
+            }
+            if(input.right) {
+                block.translate(1.0, 0, 0);
+            }
 
             sceneGraph.updateScene();
 
