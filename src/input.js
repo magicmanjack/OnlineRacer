f2e022eaef80ac71c6fda7dd986d824ea595d4cb
magicalmanjack <magicalmanjack@gmail.com>
2025-06-20 21:04:38 +1200

    Improved key event handling to be more synchronised with the frame rate.

---
+++ src/input.js
@@ -0,0 +1,94 @@
+/** Implements event queue for inputs */
+
+
+const input = {
+    upBinding : "w",
+    downBinding : "s",
+    leftBinding : "a",
+    rightBinding : "d",
+    up : false,
+    upHeld : false,
+    down : false,
+    downHeld : false,
+    left : false,
+    leftHeld : false,
+    right : false,
+    rightHeld : false,
+    eventQueue: [],
+    getTimeframeEvent : function(timestamp) {
+        
+        if(this.eventQueue.length > 0) {
+            
+            if(this.eventQueue[0].timeStamp < timestamp) {
+                return this.eventQueue.shift();
+            }
+        }
+        
+        return null;
+    },
+    processTimeframeEvents : function(frameTimeStamp) {
+        // Processes all events in the input event queue that occured before frameTimeStamp.
+        
+        for(let e = this.getTimeframeEvent(frameTimeStamp); e !== null; e = this.getTimeframeEvent(frameTimeStamp)) {
+            
+            if(e.type === "keydown") {
+                let k = e.key;
+                if(k === this.upBinding) {
+                    this.up = true;
+                    this.upHeld = true;
+                }
+                if(k === this.downBinding) {
+                    this.down = true;
+                    this.downHeld = true;
+                }
+                if(k === this.leftBinding) {
+                    this.left = true;
+                    this.leftHeld = true;
+                }
+                if(k === this.rightBinding) {
+                    this.right = true;
+                    this.rightHeld = true;
+                }
+            }
+            
+            if(e.type === "keyup") {
+                let k = e.key;
+                if(k === this.upBinding) {
+                    this.upHeld = false;
+                }
+                if(k === this.downBinding) {
+                    this.downHeld = false;
+                }
+                if(k === this.leftBinding) {
+                    this.leftHeld = false;
+                }
+                if(k === this.rightBinding) {
+                    this.rightHeld = false;
+                }
+            }
+        }
+    },
+    reset : function() {
+        //reset states if keys not held.
+        if(!this.upHeld) {
+            this.up = false;
+        }
+        if(!this.downHeld) {
+            this.down = false;
+        }
+        if(!this.rightHeld) {
+            this.right = false;
+        }
+        if(!this.leftHeld) {
+            this.left = false;
+        }
+    }
+};
+
+document.addEventListener("keydown", (event) => {
+    input.eventQueue.push(event);
+});
+
+document.addEventListener("keyup", (event) => {
+    input.eventQueue.push(event);
+});
--- src/main.js
+++ src/main.js
@@ -5,13 +5,12 @@ const MS_PER_UPDATE = 1000 / UPDATES_PER_SECOND;
 
 let gl;
 
-let block1, block2, tire;
-
 let camera = new Camera([0, 0, 60.0], [0, 0, 0]); 
 
 if(glSetup()) {
     init();
 }
+
 
 function glSetup() {
     const canvas = document.querySelector("#c");
@@ -28,27 +27,6 @@ function glSetup() {
 }
 
 function init() {
-    block1 = new SceneNode();
-    block1.mesh = new Mesh(gl, ['models/suzzane.obj']);
-    block1.translate(0, 0, -400);  
-    block1.scaleBy(100.0, 100.0, 100.0);
-    sceneGraph.root.addChild(block1);
-    
-    /*
-    block2 = new SceneNode();
-    block2.mesh = new Mesh(gl, ['models/cube.obj'], "textures/cubetexture.png");
-    block2.translate(100, 0, -300);
-    block2.scaleBy(40.0, 40.0, 40.0);
-    sceneGraph.root.addChild(block2);
-    */
-
-    tire = new SceneNode();
-    tire.mesh = new Mesh(gl, ['models/wheel.obj'], "textures/wheel.png");
-    tire.translate(0, 0, 2);
-    tire.rotate(Math.PI/2, 0, 0);
-    tire.scaleBy(1.0, 1.0, 1.0);
-    block1.addChild(tire);
-    
     requestAnimationFrame(render);
 }
 
@@ -60,20 +38,35 @@ function update(timestamp) {
         lastTime = timestamp;
     } else {
         delta += (timestamp - lastTime) / MS_PER_UPDATE;
+        lastTime = timestamp;
+
         while(delta >= 1) {
             //Process game updates
-            block1.rotate(0, 0.1, 0);
-            tire.rotate(0, 0, -0.2);
+            //Calculate a timestamp for the frame number between the last update to now.
+            
+            let frameTimeStamp = timestamp - ((delta - 1) * MS_PER_UPDATE);
+            // e.g if delta = 1 that means at the timestamp is this instant. 
+            // If delta = 2, the first frame is at (now - 1 * MS_PER_UPDATE).
+            
+
+            input.processTimeframeEvents(frameTimeStamp);
+
+            //Update objects here
+
             sceneGraph.updateScene();
+
+            input.reset();
+            
             delta--;
         }
-        lastTime = timestamp;
+        input.reset();
+        
     }
 }
 
-function render(timestamp) {
+function render() {
 
-    update(timestamp);
+    update(performance.now());
 
     gl.clearColor(0.5, 0.5, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
