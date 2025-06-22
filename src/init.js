d1cdd5e69a11d825bbefcf15465e08ab3210c65e
Tristan Kitto <kittr801@student.otago.ac.nz>
2025-06-23 01:06:57 +1200

    Added car physics & test track

--- src/init.js
+++ src/init.js
@@ -4,16 +4,96 @@ X axis to the right.
 Y axis upwards.
 origin bottom left.
 */
-let camera = new Camera([0, 0, 0], [0, 0, 0]);
+let camera = new Camera([0, 10, 15], [0, 0, 0]);
 
 let tire;
 
 function init() {
+    camera.translate(0, 10, 0);
     tire = new SceneNode();
     tire.scaleBy(5, 5, 5);
-    tire.translate(0, 0, -30);
-    tire.rotate(Math.PI/2, 0, 0);
-    tire.update = () => {tire.rotate(0, 0.1, 0.1);};
+    tire.translate(0, 0, -50);
+    tire.rotate(Math.PI / 2, 0, 0);
+
+    let tireDirection = [0, 0, -1];
+    let velocity = 0;
+    let rotateSpeed = 0;
+    let maxRotateSpeed = 0.05;
+
+    function rotateSpeedFunction(x) {
+        return x >= 0 ? ((1.2 * x - 1.2) / (1 + Math.abs(1.2 * x - 1.2)) + 0.55) / 1.5 :
+            -1 * ((0.5 * -x - 5) / (1 + Math.abs(0.5 * -x - 5)) + 0.9) / 0.12222;
+    }
+
+    tire.update = () => {
+        if (input.up) {
+            velocity += 0.3;
+            if (velocity > 17) {
+                velocity = 17;
+            }
+        }
+        if (input.down) {
+            velocity -= 0.5;
+            if (velocity < -3) {
+                velocity = -3;
+            }
+        }
+        if (input.left && Math.abs(velocity) > 0.5) {
+            tire.rotate(0, rotateSpeed, 0);
+            camera.rotate(0, rotateSpeed, 0);
+            cameraDisp = mat.subtract(camera.translation, tire.translation)
+            cameraDisp.push(1)
+            newPos = mat.multiplyVec(mat.rotate(0, rotateSpeed, 0), cameraDisp);
+            newPos.pop();
+            camera.translation = mat.add(newPos, tire.translation)
+
+            tireDirection.push(1)
+            tireDirection = mat.multiplyVec(mat.rotate(0, rotateSpeed, 0), tireDirection)
+            tireDirection.pop()
+        }
+        if (input.right && Math.abs(velocity) > 0.5) {
+            tire.rotate(0, -rotateSpeed, 0);
+            camera.rotate(0, -rotateSpeed, 0);
+            cameraDisp = mat.subtract(camera.translation, tire.translation)
+            cameraDisp.push(1)
+            newPos = mat.multiplyVec(mat.rotate(0, -rotateSpeed, 0), cameraDisp);
+            newPos.pop();
+            camera.translation = mat.add(newPos, tire.translation)
+
+            tireDirection.push(1)
+            tireDirection = mat.multiplyVec(mat.rotate(0, -rotateSpeed, 0), tireDirection)
+            tireDirection.pop()
+        }
+
+        newTireDirection = mat.scaleVec(velocity, tireDirection);
+        tire.translate(newTireDirection[0], newTireDirection[1], newTireDirection[2]);
+        camera.translate(newTireDirection[0], newTireDirection[1], newTireDirection[2]);
+        if (velocity > 0) {
+            velocity -= 0.1;
+            if (velocity < 0) {
+                velocity = 0;
+            }
+        }
+        if (velocity < 0) {
+            velocity += 0.1;
+            if (velocity > 0) {
+                velocity = 0;
+            }
+        }
+
+        if (velocity == 0) {
+            rotateSpeed = 0;
+        } else {
+            rotateSpeed = maxRotateSpeed * rotateSpeedFunction(velocity);
+        }
+    };
     tire.mesh = new Mesh(["models/wheel.obj"], "textures/wheel.png");
+
+    ground = new SceneNode();
+    ground.mesh = new Mesh(["models/ground.obj"], "textures/track.png");
+    ground.translate(0, -15, -50);
+    ground.scaleBy(500, 500, 500);
+
     sceneGraph.root.addChild(tire);
+    sceneGraph.root.addChild(ground);
 }
--- src/matrix.js
+++ src/matrix.js
@@ -1,5 +1,5 @@
 const mat = {
-    identity : function() {
+    identity: function () {
         return [
             1, 0, 0, 0,
             0, 1, 0, 0,
@@ -7,7 +7,7 @@ const mat = {
             0, 0, 0, 1
         ];
     },
-    scale : function(x, y, z) {
+    scale: function (x, y, z) {
         return [
             x, 0, 0, 0,
             0, y, 0, 0,
@@ -15,45 +15,66 @@ const mat = {
             0, 0, 0, 1
         ];
     },
-    multiply: function(a, b) {
+    scaleVec: function (a, v) {
+        if (v.length != 3) {
+            console.log("Improper vector scaling: can only have 3x1");
+            return;
+        }
+        return [a * v[0], a * v[1], a * v[2]];
+    },
+    add: function (a, b) {
+        if (a.length != 3 || b.length != 3) {
+            console.log("Improper vector addition: can only have 3x1");
+            return;
+        }
+        return [a[0] + b[0], a[1] + b[1], a[2] + b[2]];
+    },
+    subtract: function (a, b) {
+        if (a.length != 3 || b.length != 3) {
+            console.log("Improper vector subtraction: can only have 3x1");
+            return;
+        }
+        return [a[0] - b[0], a[1] - b[1], a[2] - b[2]]
+    },
+    multiply: function (a, b) {
         //Multiplies matrices a and b. (4x4)
-        
-        if(a.length != 16 || b.length != 16) {
+
+        if (a.length != 16 || b.length != 16) {
             console.log("Can only multiply (4x4)*(4x4)");
             return;
         }
-        
+
         let result = new Array(16).fill(0);
-        
-        for(let row = 0; row < 4; row++) {
-            for(let col = 0; col < 4; col++) {
-                for(let index = 0; index < 4; index++) {
-                    result[row * 4 + col] += a[row*4 + index] * b[index*4 + col];
+
+        for (let row = 0; row < 4; row++) {
+            for (let col = 0; col < 4; col++) {
+                for (let index = 0; index < 4; index++) {
+                    result[row * 4 + col] += a[row * 4 + index] * b[index * 4 + col];
                 }
             }
         }
 
         return result;
-        
+
     },
-    multiplyVec : function(m, x) {
+    multiplyVec: function (m, x) {
         //multiplies vector x by matrix m. Only for (4x4) * (4*1).
-        if(m.length != 16 || x.length != 4) {
+        if (m.length != 16 || x.length != 4) {
             console.log("Must multiply only (4x4) * (4x1)");
             return;
         }
 
         let result = new Array(4).fill(0);
 
-        for(let row = 0; row < 4; row++) {
-            for(let index = 0; index < 4; index++) {
+        for (let row = 0; row < 4; row++) {
+            for (let index = 0; index < 4; index++) {
                 result[row] += m[row * 4 + index] * x[index];
             }
         }
         return result;
     },
 
-    translate : function(tx, ty, tz) {
+    translate: function (tx, ty, tz) {
         return [
             1, 0, 0, tx,
             0, 1, 0, ty,
@@ -62,7 +83,7 @@ const mat = {
         ];
     },
 
-    rotateX: function(rx) {
+    rotateX: function (rx) {
         return [
             1, 0, 0, 0,
             0, Math.cos(rx), -Math.sin(rx), 0,
@@ -71,7 +92,7 @@ const mat = {
         ];
     },
 
-    rotateY: function(ry) {
+    rotateY: function (ry) {
         return [
             Math.cos(ry), 0, Math.sin(ry), 0,
             0, 1, 0, 0,
@@ -80,7 +101,7 @@ const mat = {
         ];
     },
 
-    rotateZ: function(rz) {
+    rotateZ: function (rz) {
         return [
             Math.cos(rz), -Math.sin(rz), 0, 0,
             Math.sin(rz), Math.cos(rz), 0, 0,
@@ -89,38 +110,38 @@ const mat = {
         ]
     },
 
-    rotate : function(rx, ry, rz) {
+    rotate: function (rx, ry, rz) {
         //creates rotation matrix that rotates anticlockwise about x then y, then z.
         //OpenGL uses a right handed coordinate system. Y up, X right, and Z out of the screen.
 
         return this.multiply(this.multiply(this.rotateZ(rz), this.rotateY(ry)), this.rotateX(rx));
     },
 
-    transpose: function(m) {
+    transpose: function (m) {
         //returns the transpose of the 4x4 matrix m.
-        if(m.length != 16) {
+        if (m.length != 16) {
             console.log("error: can only transpose a 4x4 matrix");
             return;
         }
 
         let result = new Array(16);
-        for(let row = 0; row < 4; row++) {
-            for(let col = 0; col < 4; col++) {
+        for (let row = 0; row < 4; row++) {
+            for (let col = 0; col < 4; col++) {
                 result[col * 4 + row] = m[row * 4 + col];
             }
         }
         return result;
     },
-    
-    projection : function(width, height, zNear, zFar) {
+
+    projection: function (width, height, zNear, zFar) {
         //Creates a projection matrix. 
         //width and height describe the dimensions of the zNear plane.
         //zNear and zFar must be supplied as positive values.
         return [
-            2*zNear/width, 0,              0,                              0,
-            0,             2*zNear/height, 0,                              0, 
-            0,             0,              -(zFar + zNear)/(zFar - zNear), -2*zFar*zNear/(zFar-zNear),
-            0,             0,              -1,                             0
+            2 * zNear / width, 0, 0, 0,
+            0, 2 * zNear / height, 0, 0,
+            0, 0, -(zFar + zNear) / (zFar - zNear), -2 * zFar * zNear / (zFar - zNear),
+            0, 0, -1, 0
         ];
     }
 };
