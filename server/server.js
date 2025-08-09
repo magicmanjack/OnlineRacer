f779e4a3a0de4294395748dabc519a5424d03b68
magicalmanjack <magicalmanjack@gmail.com>
2025-08-09 23:00:29 +1200

    tidied up

--- server/server.js
+++ server/server.js
@@ -161,23 +161,6 @@ function onConnectionClose(event) {
             id:playerId
         }));
     }
-
-    /*
-    for(let i = 0; i < players.length; i++) {
-        if(players[i].socket === this) {
-            for(let j = 0; j < players.length; j++) {
-                if(i !== j) {
-                    players[j].socket.send(JSON.stringify({
-                        type: "remove_player",
-                        id: players[i].player.id
-                    }));
-                }
-            }
-            players.splice(i, 1);
-            break;
-        }
-    }
-    */
 }
 
 function onPlayerMessage(event) {
@@ -188,49 +171,7 @@ function onPlayerMessage(event) {
     const msg = JSON.parse(event.data);
 
     switch(msg.type) {
-        /*
-        case "get_unique_id":
-            this.send(JSON.stringify({
-                type:"set_id",
-                id:availableId
-            }));
-            availableId++;
-            break;
-        case "add_player":
-            players.push({
-                player:msg.player,
-                socket:this,
-            });
-            for(let i = 0; i < players.length - 1; i++) {
-                players[i].socket.send(JSON.stringify({
-                    type: "add_player",
-                    player: players[players.length-1].player
-                }));
-                players[players.length-1].socket.send(JSON.stringify({
-                    type: "add_player",
-                    player: players[i].player
-                }));
-            }
-            break;
-        case "player_update":
-            for(let i = 0; i < players.length; i++) {
-                if(players[i].socket !== this) {
-                    players[i].socket.send(JSON.stringify({
-                        type:"player_update",
-                        player:msg.player
-                    }));
-                } else {
-                    players[i].player = msg.player;
-                }
-            }
-            break;
-        case "get_num_players":
-            this.send(JSON.stringify({
-                type:"num_players",
-                numPlayers:numConnected
-            }));
-            break;
-        */
+        
         case "add_car": {
             const outMsg = {
                 type:"add_car",
