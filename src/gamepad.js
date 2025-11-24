808b0941471192978265c228456a030bfbeb3445
Luke Tang <tanlu824@student.otago.ac.nz>
2025-11-24 14:12:08 +1300

    Add basic gamepad support for main menu UI

--- src/gamepad.js
+++ src/gamepad.js
@@ -5,95 +5,190 @@
  *
  */
 
-const gamepadAPI = {
-    controller: {},
-    turbo: false,
+let gamepadsState = null;
+
+const currentGamepad = {
+    index: null,
+
+    // Assume this is an Xbox 360 Controller for now (17 buttons)
+    // PS4/PS5 controllers will have an extra button for the touchpad (18 buttons)
     buttons: [
-        "A",
-        "B",
-        "X",
-        "Y",
-        "LB",
-        "RB",
-        "LT",
-        "RT",
-        "Back",
-        "Start",
-        "LS",
-        "RS",
-        "DPad-Up",
-        "DPad-Down",
-        "DPad-Left",
-        "DPad-Right",
-        "Xbox Guide"
+        false, // A
+        false, // B
+        false, // X
+        false, // Y
+        false, // LB
+        false, // RB
+        false, // LT
+        false, // RT
+        false, // Back
+        false, // Start
+        false, // LS
+        false, // RS
+        false, // DPad-Up
+        false, // DPad-Down
+        false, // DPad-Left
+        false, // DPad-Right
+        false, // Xbox Guide
+        // false, // PS4/PS5 touchpad
     ],
-    buttonsCache: [],
-    buttonsStatus: [],
-    axesStatus: [],
-    connect(evt) {
-        gamepadAPI.controller = evt.gamepad;
-        gamepadAPI.turbo = true;
-        console.log("Gamepad connected.");
-    },
-    disconnect(evt) {
-        gamepadAPI.turbo = false;
-        delete gamepadAPI.controller;
-        console.log("Gamepad disconnected.");
-    },
+
+    buttonMappings: new Map([
+        [0, "A"],
+        [1, "B"],
+        [2, "X"],
+        [3, "Y"],
+        [4, "LB"],
+        [5, "RB"],
+        [6, "LT"],
+        [7, "RT"],
+        [8, "Back"],
+        [9, "Start"],
+        [10, "LS"],
+        [11, "RS"],
+        [12, "DPad-Up"],
+        [13, "DPad-Down"],
+        [14, "DPad-Left"],
+        [15, "DPad-Right"],
+        [16, "Xbox Guide"],
+    ]),
+
+    buttonStates: new Map([
+        ["A", false],
+        ["B", false],
+        ["X", false],
+        ["Y", false],
+        ["LB", false],
+        ["RB", false],
+        ["LT", false],
+        ["RT", false],
+        ["Back", false],
+        ["Start", false],
+        ["LS", false],
+        ["RS", false],
+        ["DPad-Up", false],
+        ["DPad-Down", false],
+        ["DPad-Left", false],
+        ["DPad-Right", false],
+        ["Xbox Guide", false],
+    ]),
+
+    timestamp: 0,
+
     update() {
-        // Clear the buttons cache
-        gamepadAPI.buttonsCache = [];
+        gamepadsState = navigator.getGamepads();
 
-        // Move the buttons status from the previous frame to the cache
-        for (let k = 0; k < gamepadAPI.buttonsStatus.length; k++) {
-            gamepadAPI.buttonsCache[k] = gamepadAPI.buttonsStatus[k];
-        }
+        const activeGamepad = gamepadsState[this.index];
 
-        // Clear the buttons status
-        gamepadAPI.buttonsStatus = [];
+        if (activeGamepad && activeGamepad.timestamp !== this.timestamp) {
+            this.timestamp = activeGamepad.timestamp;
 
-        // Get the gamepad object
-        const c = gamepadAPI.controller || {};
-
-        // Loop through buttons and push the pressed ones to the array
-        const pressed = [];
-        if (c.buttons) {
-            for (let b = 0; b < c.buttons.length; b++) {
-                if (c.buttons[b].pressed) {
-                    pressed.push(gamepadAPI.buttons[b]);
-                }
+            for (let i = 0; i < this.buttons.length; i++) {
+                this.buttons[i] = activeGamepad.buttons[i].pressed;
+                this.buttonStates.set(
+                    this.buttonMappings.get(i),
+                    this.buttons[i]
+                );
             }
+        } else {
+            return;
         }
-
-        // Loop through axes and push their values to the array
-        const axes = [];
-        if (c.axes) {
-            for (const ax of c.axes) {
-                axes.push(ax.toFixed(2));
-            }
-        }
-
-        // Assign received values
-        gamepadAPI.axesStatus = axes;
-        gamepadAPI.buttonsStatus = pressed;
-
-        // Return buttons for debugging purposes
-        return pressed;
     },
-    buttonPressed(button, hold) {
-        let newPress = false;
-        if (!gamepadAPI.controller) {
-            return newPress;
-        }
-        if (gamepadAPI.buttons.includes(button)) {
-            newPress = true;
-        }
-        if (!hold && gamepadAPI.buttons.includes(button)) {
-            newPress = false;
-        }
-        return newPress;
-    },
+    //     controller: {},
+    //     turbo: false,
+    //     buttons: [
+    //         "A",
+    //         "B",
+    //         "X",
+    //         "Y",
+    //         "LB",
+    //         "RB",
+    //         "LT",
+    //         "RT",
+    //         "Back",
+    //         "Start",
+    //         "LS",
+    //         "RS",
+    //         "DPad-Up",
+    //         "DPad-Down",
+    //         "DPad-Left",
+    //         "DPad-Right",
+    //         "Xbox Guide"
+    //     ],
+    //     buttonsCache: [],
+    //     buttonsStatus: [],
+    //     axesStatus: [],
+    //     connect(evt) {
+    //         gamepadAPI.controller = evt.gamepad;
+    //         gamepadAPI.turbo = true;
+    //         console.log("Gamepad connected.");
+    //     },
+    //     disconnect(evt) {
+    //         gamepadAPI.turbo = false;
+    //         delete gamepadAPI.controller;
+    //         console.log("Gamepad disconnected.");
+    //     },
+    //     update() {
+    //         // Clear the buttons cache
+    //         gamepadAPI.buttonsCache = [];
+
+    //         // Move the buttons status from the previous frame to the cache
+    //         for (let k = 0; k < gamepadAPI.buttonsStatus.length; k++) {
+    //             gamepadAPI.buttonsCache[k] = gamepadAPI.buttonsStatus[k];
+    //         }
+
+    //         // Clear the buttons status
+    //         gamepadAPI.buttonsStatus = [];
+
+    //         // Get the gamepad object
+    //         const c = gamepadAPI.controller || {};
+
+    //         // Loop through buttons and push the pressed ones to the array
+    //         const pressed = [];
+    //         if (c.buttons) {
+    //             for (let b = 0; b < c.buttons.length; b++) {
+    //                 if (c.buttons[b].pressed) {
+    //                     pressed.push(gamepadAPI.buttons[b]);
+    //                 }
+    //             }
+    //         }
+
+    //         // Loop through axes and push their values to the array
+    //         const axes = [];
+    //         if (c.axes) {
+    //             for (const ax of c.axes) {
+    //                 axes.push(ax.toFixed(2));
+    //             }
+    //         }
+
+    //         // Assign received values
+    //         gamepadAPI.axesStatus = axes;
+    //         gamepadAPI.buttonsStatus = pressed;
+
+    //         // Return buttons for debugging purposes
+    //         return pressed;
+    //     },
+    //     buttonPressed(button, hold) {
+    //         let newPress = false;
+    //         if (!gamepadAPI.controller) {
+    //             return newPress;
+    //         }
+    //         if (gamepadAPI.buttons.includes(button)) {
+    //             newPress = true;
+    //         }
+    //         if (!hold && gamepadAPI.buttons.includes(button)) {
+    //             newPress = false;
+    //         }
+    //         return newPress;
+    //     },
 };
 
-window.addEventListener("gamepadconnected", gamepadAPI.connect);
-window.addEventListener("gamepaddisconnected", gamepadAPI.disconnect);
+// window.addEventListener("gamepadconnected", (event) => {
+//     console.log("Gamepad connected.");
+//     console.log(event.gamepad);
+// });
+
+// window.addEventListener("gamepaddisconnected", (event) => {
+//     console.log("Gamepad disconnected.");
+//     console.log(event.gamepad);
+// });
