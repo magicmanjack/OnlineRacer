abe046afaef089c1fd38b31080e511cd23fcef3f
Luke Tang <tanlu824@student.otago.ac.nz>
2025-11-26 13:10:10 +1300

    Raise default master volume

--- src/track.js
+++ src/track.js
@@ -58,14 +58,14 @@ function loadTrack1() {
     var soundBuffer = null;
     var audioContext = new AudioContext();
 
-    function loadAudio(elementId, volume = 0.15) {
+    function loadAudio(elementId, volume = 0.25) {
         // Load file from audio element
         const audioElement = document.getElementById(elementId);
         const track = audioContext.createMediaElementSource(audioElement);
 
-        // Set default volume
-        const gainNode = audioContext.createGain();
-        gainNode.gain.value = volume;
+        // // Set default volume
+        // const gainNode = audioContext.createGain();
+        // gainNode.gain.value = volume;
 
         // Add modifier based on volume slider (0% to 200% of default volume value)
         const volumeControlGainNode = audioContext.createGain();
@@ -75,7 +75,7 @@ function loadTrack1() {
             volumeControlGainNode.gain.value = volumeControl.value;
         });
 
-        track.connect(gainNode).connect(volumeControlGainNode).connect(audioContext.destination);
+        track/*.connect(gainNode)*/.connect(volumeControlGainNode).connect(audioContext.destination);
 
         return audioElement;
     }
